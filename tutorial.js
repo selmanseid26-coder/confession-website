20e835c5f79ddb57e3d6e967d6cfebbc05d96348
selmanseid26-coder <selmanseid26@gmail.com>
2025-11-15 22:06:18 +0300

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,62 @@
+// VARIABLES
+let name = "selman";
+const country = "ethiopia";
+var age = 22;
+// practice
+const favorite sport ="football";
+let current skill ="beginer";
+let hours to code = 2;
+
+// console.log
+
+console.log("Hello world!");
+
+// from the previous lesson
+
+let name = "selman";
+console.log(name);
+// practice
+
+const city = "Addis Ababa";
+let goal = "get more active income";
+console.log(city);
+console.log(goal);
+
+// numbers and math
+
+let age = 22;
+let score = 50;
+let price = 60;
+let size = 54;
+// no quotes cuz quotes are for text
+
+// maths
+ let x = 30;
+ let y = 50;
+
+ console.log(x + y);
+ console.log(x - y);
+ console.log( x * y);
+ console.log( x / y);
+ console.log(x % y);
+
+ // real life example
+
+ let money = 500;
+ let meal = 400;
+
+ let remaining = money - meal;
+
+ console.log(remaining);
+
+
+ // practice
+
+ let t =6;
+ let y =5;
+
+ console.log(t + y);
+ console.log(t - y);
+ console.log(t * y);
+ console.log(t / y);
+ 
